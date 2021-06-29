c741ec25d5187698a5914686635a2d0975acb369
XuSong Lin (NCS) <xusong.lin@ncs.com.sg>
2021-06-29 14:59:19 +0800

    fail the test case

--- test/test/MyStringStackTest.java
+++ test/test/MyStringStackTest.java
@@ -32,11 +32,14 @@ public class MyStringStackTest extends TestCase {
 	@Test
 	public void testPush() {
 		String o1 = null;  
+		//MyStringStack stack = new MyStringStack();
+		//assertTrue (stack.isEmpty());
+		//stack.push (s1);
+        //stack.push (s2);
+        //assertFalse (stack.isEmpty());
 		MyStringStack stack = new MyStringStack();
-		assertTrue (stack.isEmpty());
 		stack.push (s1);
-        stack.push (s2);
-        assertFalse (stack.isEmpty());
+		assertTrue (stack.isEmpty());
 	}
 
 	@Test
